eb28c13820afe9d1205d872ce78ffa296164f22a
Baburin Sergey <sbasba777@hotmail.com>
2015-05-07 21:21:29 +0300

    Add currencies

---
+++ src/main/Currency.java
@@ -0,0 +1,18 @@
+package main;
+
+enum Currency {
+
+    USD("R01235"),
+    EUR("R01239"),
+    RUB("n/a");
+
+    private final String cbrCode;
+
+    Currency(String cbrCode) {
+        this.cbrCode = cbrCode;
+    }
+
+    String getCbrCode() {
+        return cbrCode;
+    }
+}
--- src/main/ExchangeRates.java
+++ src/main/ExchangeRates.java
@@ -1,4 +1,13 @@
 package main;
 
+import static main.Currency.*;
+
 public class ExchangeRates {
+
+    public static void main(String... args) {
+
+        for (Currency currency : Currency.values()) {
+            System.out.printf("%s, cbr code: %s%n", currency.name(), currency.getCbrCode());
+        }
+    }
 }
