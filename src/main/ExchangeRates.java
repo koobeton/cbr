90898960a616e55281ea3e2c6ffcfe474096782d
Baburin Sergey <sbasba777@hotmail.com>
2015-05-08 18:31:59 +0300

    Set dates

--- src/main/ExchangeRates.java
+++ src/main/ExchangeRates.java
@@ -1,15 +1,36 @@
 package main;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import static main.Currency.*;
 
 public class ExchangeRates {
 
+    private static final String DATE_FORMAT = "dd/MM/yyyy";
+
+    private static String today, yesterday;
+
     public static void main(String... args) {
+
+        setDates();
 
         for (Currency currency : Currency.values()) {
             if (currency.getCbrCode() != null) {
                 System.out.printf("%s, cbr code: %s%n", currency.name(), currency.getCbrCode());
             }
         }
+
+        System.out.println(today);
+        System.out.println(yesterday);
+    }
+
+    private static void setDates() {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+        Calendar calendar = Calendar.getInstance();
+        today = dateFormat.format(calendar.getTime());
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        yesterday = dateFormat.format(calendar.getTime());
     }
 }
