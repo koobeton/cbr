f721787861e053f1f6498eee41dce71185a1067a
Baburin Sergey <sbasba777@hotmail.com>
2015-11-07 22:56:54 +0300

    Add check for available currencies

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,8 +2,11 @@ package main;
 
 import exchange.Currency;
 import exchange.ExchangeRates;
+import org.fusesource.jansi.AnsiConsole;
 
 import java.util.Map;
+
+import static misc.Paint.*;
 
 public class Main {
 
@@ -15,7 +18,15 @@ public class Main {
             ExchangeRates.print(rates.values());
         } else {
             for (String arg : args) {
-                ExchangeRates.print(rates.get(arg.toUpperCase()));
+                if (rates.containsKey(arg.toUpperCase())) {
+                    ExchangeRates.print(rates.get(arg.toUpperCase()));
+                } else {
+                    AnsiConsole.systemInstall();
+                    System.out.printf("%s %s%n",
+                            getAnsiString(MAGENTA, arg.toUpperCase()),
+                            getAnsiString(RED, "no such currency"));
+                    AnsiConsole.systemUninstall();
+                }
             }
         }
     }
